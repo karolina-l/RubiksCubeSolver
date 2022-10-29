7df5498f8c009bb0ddd992d3e7e039df05c92cf0
karolina-l <57325023+karolina-l@users.noreply.github.com>
2022-10-29 22:28:08 +0200

    Add ROI, slice and grid. Improve color ranges

--- color_ranges.py
+++ color_ranges.py
@@ -2,25 +2,28 @@ import numpy as np
 
 
 # Set range for red color
-red_lower = np.array([136, 87, 111], np.uint8)
-red_upper = np.array([180, 255, 255], np.uint8)
+red_lower_1 = np.array([0,77,77])
+red_upper_1 = np.array([8,255,255])
+
+red_lower_2 = np.array([165,77,77])
+red_upper_2 = np.array([179,255,255 ])
 
 # Set range for green color
-green_lower = np.array([25, 52, 72], np.uint8)
-green_upper = np.array([102, 255, 255], np.uint8)
+green_lower = np.array([32,77,77])
+green_upper = np.array([79,255,255 ])
 
 # Set range for blue color
-blue_lower = np.array([94, 80, 2], np.uint8)
-blue_upper = np.array([120, 255, 255], np.uint8)
+blue_lower = np.array([80,77,77])
+blue_upper = np.array([130,255,255 ])
 
 # Set range for yellow color
-yellow_lower = np.array([20, 80, 80], np.uint8)
-yellow_upper = np.array([30, 255, 255], np.uint8)
+yellow_lower = np.array([23,77,77 ])
+yellow_upper = np.array([31,255,255 ])
 
 # Set range for white color
-white_lower = np.array([200, 200, 200], np.uint8)
-white_upper = np.array([255, 255, 255], np.uint8)
+white_lower = np.array([0,0,100])
+white_upper = np.array([179,76,255 ])
 
 # Set range for orange color
-orange_lower = np.array([0, 70, 125], np.uint8)
-orange_upper = np.array([100, 160, 255], np.uint8)
+orange_lower = np.array([9,77,77])
+orange_upper = np.array([22,255,255 ])
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import numpy as np
 import cv2
 import color_ranges as cr
+import copy
 
 # Capturing video through webcam
-webcam = cv2.VideoCapture(0)
+# webcam = cv2.VideoCapture(0)
 
 
 def DilationAndMask(color_mask, imageFrame):
@@ -31,9 +32,9 @@ def Contour(color_mask, color_name, color_r, color_g, color_b, frame):
                                        (x + w, y + h),
                                        (color_b, color_g, color_r), 2)
 
-            cv2.putText(frame, color_name + " Color", (x, y),
+            cv2.putText(frame, f"{color_name} Color", (x, y),
                         cv2.FONT_HERSHEY_SIMPLEX, 1.0,
-                        (color_b, color_g, color_r))
+                        (0, 0, 0))
 
 
 img_ctr = 0
@@ -46,60 +47,85 @@ while (1):
 
     # Reading the video from the
     # webcam in image frames
-    # cv2.namedWindow("test")
-    while (img_ctr < 6):
-        ret, imageFrame = webcam.read()
-        imageFrame = cv2.rectangle(imageFrame, grid_start, grid_end, grid_color, grid_thickness)
-        if not ret:
-            print("failed to grab frame")
-            break
-        cv2.imshow("Rubiks Cube Solver", imageFrame)
-        action = cv2.waitKey(1)
-        if action%256 == 32:
-            captured_img = f"./Images/cube_{img_ctr}.png"
-            cv2.imwrite(captured_img, imageFrame)
-            print(f"{captured_img} written!")
-            img_ctr += 1
+    # # cv2.namedWindow("test")
+    # while (img_ctr < 6):
+    #     ret, imageFrame = webcam.read()
+    #     img_cpy = copy.copy(imageFrame)
+    #     img_cpy = cv2.rectangle(img_cpy, grid_start, grid_end, grid_color, grid_thickness)
+    #     if not ret:
+    #         print("failed to grab frame")
+    #         break
+    #     cv2.imshow("Rubiks Cube Solver", img_cpy)
+    #     action = cv2.waitKey(1)
+    #     if action%256 == 32:
+    #         captured_img = f"./Images/cube_{img_ctr}.png"
+    #         cv2.imwrite(captured_img, imageFrame)
+    #         print(f"{captured_img} written!")
+    #         img_ctr += 1
 
-    # print("idziemy dalej")
-    # for i in range(5):
-    #     path = f'./Images/cube_{i}.png'
-    #     cube_img = cv2.imread(path)
+    print("idziemy dalej")
+    # for i in range(6):
+    #     # path = f'./Images/cube_{i}.png'
+    #     # cube_img = cv2.imread(path)
+    #     # cv2.imshow("Rubiks Cube Solver", cube_img)
+    #     #
+    #     # isFromCenter = False
+    #     # r = cv2.selectROI("Rubiks Cube Solver", cube_img, isFromCenter)
+    #     # roi_img = cube_img[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
+    #     # captured_img = f"./Images/roi_{i}.png"
+    #     # cv2.imwrite(captured_img, roi_img)
+    #
     #     # Convert the imageFrame in
     #     # BGR(RGB color space) to
     #     # HSV(hue-saturation-value)
     #     # color space
-    #     hsvFrame = cv2.cvtColor(cube_img, cv2.COLOR_BGR2HSV)
-    #
-    #     # define masks
-    #     red_mask = cv2.inRange(hsvFrame, cr.red_lower, cr.red_upper)
-    #     green_mask = cv2.inRange(hsvFrame, cr.green_lower, cr.green_upper)
-    #     blue_mask = cv2.inRange(hsvFrame, cr.blue_lower, cr.blue_upper)
-    #     yellow_mask = cv2.inRange(hsvFrame, cr.yellow_lower, cr.yellow_upper)
-    #     white_mask = cv2.inRange(hsvFrame, cr.white_lower, cr.white_upper)
-    #     orange_mask = cv2.inRange(hsvFrame, cr.orange_lower, cr.orange_upper)
-    #
-    #     # Morphological Transform, Dilation
-    #     red_mask, res_red = DilationAndMask(red_mask, cube_img)
-    #     green_mask, res_green = DilationAndMask(green_mask, cube_img)
-    #     blue_mask, res_blue = DilationAndMask(blue_mask, cube_img)
-    #     yellow_mask, res_yellow = DilationAndMask(yellow_mask, cube_img)
-    #     white_mask, res_white = DilationAndMask(white_mask, cube_img)
-    #     orange_mask, res_orange = DilationAndMask(orange_mask, cube_img)
-    #
-    #     # Creating contours to track colors
-    #     Contour(red_mask, "Red", 255, 0, 0, cube_img)
-    #     Contour(green_mask, "Green", 0, 255, 0, cube_img)
-    #     Contour(blue_mask, "Blue", 0, 0, 255, cube_img)
-    #     Contour(yellow_mask, "Yellow", 255, 255, 0, cube_img)
-    #     Contour(white_mask, "White", 255, 255, 255, cube_img)
-    #     Contour(orange_mask, "Orange", 255, 128, 0, cube_img)
-    #
-    #     cv2.imshow("Rubiks Cube Solver", cube_img)
-    #     cv2.waitKey(0)
+    #     path = f'./Images/roi_{i}.png'
+    #     roi_img = cv2.imread(path)
+    #     hsvFrame = cv2.cvtColor(roi_img, cv2.COLOR_BGR2HSV)
+
+        # define masks
+        # red_mask = cv2.inRange(hsvFrame, cr.red_lower, cr.red_upper)
+        # green_mask = cv2.inRange(hsvFrame, cr.green_lower, cr.green_upper)
+        # blue_mask = cv2.inRange(hsvFrame, cr.blue_lower, cr.blue_upper)
+        # yellow_mask = cv2.inRange(hsvFrame, cr.yellow_lower, cr.yellow_upper)
+        # white_mask = cv2.inRange(hsvFrame, cr.white_lower, cr.white_upper)
+        # orange_mask = cv2.inRange(hsvFrame, cr.orange_lower, cr.orange_upper)
+        #
+        # # Morphological Transform, Dilation
+        # red_mask, res_red = DilationAndMask(red_mask, roi_img)
+        # green_mask, res_green = DilationAndMask(green_mask, roi_img)
+        # blue_mask, res_blue = DilationAndMask(blue_mask, roi_img)
+        # yellow_mask, res_yellow = DilationAndMask(yellow_mask, roi_img)
+        # white_mask, res_white = DilationAndMask(white_mask, roi_img)
+        # orange_mask, res_orange = DilationAndMask(orange_mask, roi_img)
+        #
+        # # Creating contours to track colors
+        # Contour(red_mask, "Red", 255, 0, 0, roi_img)
+        # Contour(green_mask, "Green", 0, 255, 0, roi_img)
+        # Contour(blue_mask, "Blue", 0, 0, 255, roi_img)
+        # Contour(yellow_mask, "Yellow", 255, 255, 0, roi_img)
+        # Contour(white_mask, "White", 255, 255, 255, roi_img)
+        # Contour(orange_mask, "Orange", 255, 128, 0, roi_img)
+        #
+        # cv2.imshow("Rubiks Cube Solver", roi_img)
+        # cv2.waitKey(0)
 
     # Program Termination
     # cv2.imshow("Multiple Color Detection in Real-Time", imageFrame)
+
+    path = './Images/roi_0.png'
+    roi_img = cv2.imread(path)
+    cv2.imshow("Rubiks Cube Solver", roi_img)
+    height, width, _ = roi_img.shape
+    print(f'{height}, {width}')
+
+    for a in range(3):
+       for b in range(3):
+           print(f'{a}, {b}')
+           slice = roi_img[int(width/3)*b:int(height/3)*(b+1), int(width/3)*a:int(height/3)*(a+1)]
+           captured_img = f"./Images/slice_{a}{b}.png"
+           cv2.imwrite(captured_img, slice)
+    cv2.waitKey(0)
 
     if cv2.waitKey(1) == 27:
         webcam.release()
